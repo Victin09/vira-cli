348f69024f138b0f10ce8730d006c55670404244
Victor Gomez <victor.gomez@ghenova.net>
2021-07-29 18:27:40 +0200

    copy template folder and install with npm

--- src/common/utils/cli.utils.ts
+++ src/common/utils/cli.utils.ts
@@ -1,7 +1,14 @@
+import path from 'path';
+import chalk from 'chalk';
+
 import ProjectAnswers from '../interfaces/answers.interface';
 import constants from '../constants/project.constants';
 import initGitRepository from './git.utils';
 import generateProjectPath from './path.utils';
+import checkProjectFolder from './folder.utils';
+import copyFileSync from './fs.template';
+import packageManagerInit from './pm.utils';
+import MESSAGES from '../constants/messages.cosntants';
 
 const generate = async (projectOptions: ProjectAnswers) => {
     switch (projectOptions.projectType.toLowerCase()) {
@@ -22,14 +29,29 @@ const generate = async (projectOptions: ProjectAnswers) => {
     }
 }
 
-const generateExpressTemplate = (projectOptions: ProjectAnswers): void => {
-    const projectPath = generateProjectPath(projectOptions.projectName);
-    
-    // copy express template
+const generateExpressTemplate = async (projectOptions: ProjectAnswers) => {
+    if (checkProjectFolder(projectOptions.projectName)) {
+        const projectPath = generateProjectPath(projectOptions.projectName);
+        // copy express template
+        let templatePathToGenerate = path.join(process.cwd(), 'src/templates/express/ts-typeorm');
+        if (!projectOptions.projectExpress!.toLowerCase().includes('typeorm')) templatePathToGenerate = path.join(process.cwd(), 'src/templates/express/ts-no-typeorm');
+        copyFileSync(templatePathToGenerate, projectPath, projectPath);
+        // init git
+        if (projectOptions.projectGit) initGitRepository(projectPath);
+        // install with package manager
+        await packageManagerInit(projectOptions.projectPackageManager, projectPath, projectOptions.projectName);
+        // Run get started messages
+        getStartedMessages(projectPath, projectOptions.projectPackageManager);
+    }
 
-    // init git
-    if (projectOptions.projectGit) initGitRepository(projectPath);
-    // install with package manager
+}
+
+const getStartedMessages = (projectPath: string, packageManager: string) => {
+    console.info(MESSAGES.GET_STARTED_INFORMATION);
+    console.info();
+    console.info(chalk.gray(MESSAGES.CHANGE_DIR_COMMAND(projectPath)));
+    console.info(chalk.gray(MESSAGES.START_COMMAND(packageManager.toLowerCase())));
+    console.info();
 }
 
 export default generate;
---
+++ src/common/utils/pm.utils.ts
@@ -0,0 +1,51 @@
+import { ChildProcess, spawn, SpawnOptions } from 'child_process';
+import ora from 'ora';
+import chalk from 'chalk';
+
+import MESSAGES from '../constants/messages.cosntants';
+
+const packageManagerInit = async (packageManager: string, projectPath: string, projectname: string) => {
+    const spinner = ora(MESSAGES.PACKAGE_MANAGER_INSTALLATION_IN_PROGRESS);
+    spinner.spinner = 'arrow3';
+    spinner.start();
+    try {
+        if (packageManager.toLowerCase().includes('npm')) {
+            const child: ChildProcess = spawn('npm', ['-v']);
+            child.stderr!.on('data', () => {
+                console.log(chalk.red(MESSAGES.PACKAGE_MANAGER_ERROR));
+                process.exit();
+            });
+            await run('npm install --silent', true, projectPath);
+        }
+        spinner.succeed(MESSAGES.PACKAGE_MANAGER_INSTALLATION_SUCCEED(projectname));
+    } catch (error) {
+        spinner.fail();
+    }
+}
+
+const run = async (command: string, collect = false, cwd: string = process.cwd()): Promise<null | string> => {
+    const args: string[] = [command];
+    const options: SpawnOptions = {
+        cwd,
+        stdio: collect ? 'pipe' : 'inherit',
+        shell: true,
+    };
+    return new Promise<null | string>((resolve, reject) => {
+        const child: ChildProcess = spawn(command, [...args], options);
+        if (collect) {
+            child.stdout!.on('data', (data) =>
+                resolve(data.toString().replace(/\r\n|\n/, '')),
+            );
+        }
+        child.on('close', (code) => {
+            if (code === 0) {
+                resolve(null);
+            } else {
+                console.error(chalk.red(MESSAGES.RUNNER_EXECUTION_ERROR(`${command}`)));
+                reject();
+            }
+        });
+    });
+}
+
+export default packageManagerInit;
